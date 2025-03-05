46f57c314402a5992222b99fd975cdab3af1ba9a
Sebastian Jimenez <anderjime864@gmail.com>
2025-03-05 13:08:29 -0500

    feat: auth

---
+++ src/infrastructure/database/migrations/0001_fuzzy_next_avengers.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"username" varchar(100) NOT NULL,
+	"email" varchar(150) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" varchar(50) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
